8f80dc096ab9553ebabfcdde297bc9b2439e27bf
Sashini Ruwanthika Weliketiya <sashiniruwanthika08@gmail.com>
2025-03-11 20:35:21 +0530

    Mathematical Opeartions

---
+++ Question2.js
@@ -0,0 +1,11 @@
+console.log("--Mathematical Opeartions--")
+
+let x = 10;
+let y = 5;
+
+let sum = x + y;
+
+console.log("Summation is: "+sum);
+console.log(x-y);
+console.log(x/y);
+console.log(x*y);
