0f4b96d1cc1c9c2c9530d8b7a0f4518ee004d13e
Sashini Ruwanthika Weliketiya <sashiniruwanthika08@gmail.com>
2025-03-21 02:48:28 +0530

    Add files via upload

---
+++ DAY_04/Codes/app.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const app=express();
+const port=3001;
+
+app.get('/',(req,res) =>{
+   res.send('Hello Expree JS');
+
+});
+
+app.get('/msg',(req,res) =>{
+    res.send('Hello IT Students');
+ 
+ });
+
+ app.listen (port,()=>{
+  console.log(`Server is running on ${port}`);
+ });
