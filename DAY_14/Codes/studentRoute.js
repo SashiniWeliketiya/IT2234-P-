09ec9fafcca0adb6af2b1ba6f1017d00e6d39761
Sashini Ruwanthika Weliketiya <sashiniruwanthika08@gmail.com>
2025-05-25 23:14:12 +0530

    Update studentRoute.js

--- DAY_14/Codes/studentRoute.js
+++ DAY_14/Codes/studentRoute.js
@@ -1,23 +1,116 @@
-const mongoose = require('mongoose')
-const studentSchema= new mongoose.Schema({
-    _id:{type:String,require:true},
-    name:{type:String,require:true},
-    date_of_birth:{type:Date},
-    gender:{type:String},
-    degreeId:{
-        type:String,
-        require:true,
-        ref:'degrees'
+const express = require('express')
+const router = express.Router()
+const student = require('../models/student')
+const { default: mongoose } = require('mongoose')
+
+router.get('/',async(req,res)=>{
+    try{
+        const results = await student.find().populate("degreeId")
+        if(results) {
+            res.status(200).json(results)
+        }else{
+            res.status(404).send("Sorry, no data found!")
+        }
+    }catch(error){
+        console.error(error);
+        res.status(500).send("Server Error!")
     }
 })
 
-const student=mongoose.model('students',studentSchema)
-const Kolins = new student({
-    _id:'2021ICT01',
-    name:'Peter Kolins',
-    date_of_birth:'01-01-1995',
-    gender:'Male',
-    degreeId:'FAS2000ICT'
+router.get('/code/:cid',async(req,res)=>{
+    try{
+        const cid = req.params.cid
+        const results = await student.find({code:cid})
+        const count = results.length
+        console.log(count)
+        if(results) {
+           if(count>0){
+                res.status(200).json(results)
+           }else{
+            res.status(404).send("Sorry, no data found!")
+           }
+        }else{
+            res.status(404).send("Sorry, no data found!")
+        }
+    }catch(error){
+        console.error(error);
+        res.status(500).send("Server Error!")
+    }
 })
-Kolins.save()
-module.exports=student
+
+//insert
+
+router.post('/',async(req,res)=>{
+    try{
+        const {_id,name,age} = req.body
+        if(!_id || !name || !age) {
+            res.status(400).send("Please provide data to the required fileds!")
+        }else{
+            const results = await student.create({_id,name,age})
+            res.status(200).json(results)
+        }
+    }catch(error){
+        console.error(error);
+        res.status(500).send("Server Error!")
+    }
+})
+
+//update
+
+router.put('/:id',async(req,res)=>{
+    try{
+        const id = req.params.id
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).send("Invalid ID!")
+        }
+        const ustudent = await student.findById(id)
+        const {_id,name,age} = req.body
+        if(!_id || !name || !age) {
+            res.status(400).send("Please provide data to the required fileds!")
+        }else{
+            const results = await ustudent.updateOne({_id,name,age})
+            res.status(200).json(results)
+        }
+    }catch(error){
+        console.error(error);
+        res.status(500).send("Server Error!")
+    }
+})
+
+//delete
+
+router.delete('/:id',async(req,res)=>{
+    try{
+        const id = req.params.id
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).send("Invalid ID!")
+        }
+        const dstudent = await student.findById(id)
+        const results = await dstudent.deleteOne(dstudent).catch((error)=>{
+            return res.status(500).json(error)}
+        )
+        res.status(200).json(results)
+    }catch(error){
+        console.error(error);
+        res.status(500).send("Server Error!")
+    }
+})
+
+router.get('/course',async(req,res)=>{
+    console.log('check')
+    try{
+        
+        const results = await student.find().populate("degreeId").populate("enroled_courses")
+        if(results) {
+            res.status(200).json(results)
+        }else{
+            res.status(404).send("Sorry, no data found!")
+        }
+    }catch(error){
+        console.error(error);
+        res.status(500).send("Server Error!")
+    }
+})
+
+module.exports=router
+
